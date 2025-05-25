9b279e396b318eb767f6ea57fe3565388e767a36
Mahilesh <87026686+Mahilesh@users.noreply.github.com>
2025-05-25 06:52:41 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull mahileshdevops/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 mahileshdevops/simple-python-flask-app
